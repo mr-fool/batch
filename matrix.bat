95a8b06ea6ca1318573cc0eab0f1d5ff241b002e
mr-fool <mr-fool@users.noreply.github.com>
2015-11-12 23:39:12 -0700

    Create matrix.bat

---
+++ matrix.bat
@@ -0,0 +1,12 @@
+@echo off
+Pause
+Color 0A
+mode 1000
+
+:number
+
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%
+%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+%random%%random%%random%%random%%random%%random%%random%%random%%random%
+%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto number
